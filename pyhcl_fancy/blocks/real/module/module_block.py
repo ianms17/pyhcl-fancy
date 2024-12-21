77be4dc19f922ebeb6ef638ab3d2d25f8770c4f6
ianms17 <ianmstephenson17@gmail.com>
2024-12-20 21:04:34 -0600

    small fix in module state path logic

--- pyhcl_fancy/blocks/real/module/module_block.py
+++ pyhcl_fancy/blocks/real/module/module_block.py
@@ -1,4 +1,5 @@
 from pyhcl_fancy.blocks.real.real_block import RealBlock
+from pyhcl_fancy.collection_tree.node import Node
 
 
 class ModuleBlock(RealBlock):
@@ -18,9 +19,13 @@ class ModuleBlock(RealBlock):
     def convert_to_hcl(self) -> str:
         pass
 
-    def parse(self, raw_module_dict: dict, module_file_path: str) -> None:
+    def parse(self, raw_module_dict: dict, module_file_path: str, parent_file_node: Node) -> None:
         self.module_name = raw_module_dict.keys()[0]
         self.file_path = module_file_path
+        if parent_file_node.submodule_state_path == None:
+            self.state_path = f"module.{self.module_name}"
+        else:
+            self.state_path = parent_file_node.submodule_state_path
         for attribute in raw_module_dict[self.module_name]:
             match attribute:
                 case "source":
