782f1127e3b61b78551dc2f77ef3c70321f3ab80
ianms17 <ianmstephenson17@gmail.com>
2024-12-21 15:51:38 -0600

    tf block test

--- pyhcl_fancy/blocks/terraform_block.py
+++ pyhcl_fancy/blocks/terraform_block.py
@@ -7,12 +7,3 @@ class TerraformBlock:
             file_path (str): The path of the file where the block is defined
         """
         self.file_path: str = ""
-
-    def _get_resource_type(self) -> str:
-        return ""
-
-    def _get_file_path(self) -> str:
-        return ""
-
-    def reverse_engineer_variables(self) -> None:
-        return None
--- tests/unit/blocks/test_terraform_block.py
+++ tests/unit/blocks/test_terraform_block.py
@@ -1,1 +1,5 @@
 from pyhcl_fancy.blocks.terraform_block import TerraformBlock
+
+def test_terraform_block_init():
+    block = TerraformBlock()
+    assert block.file_path == ""
