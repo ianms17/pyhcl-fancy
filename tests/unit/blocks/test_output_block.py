9420dbe85a4be33f8e3d811f9b06378e0d893e07
ianms17 <ianmstephenson17@gmail.com>
2024-12-30 16:41:52 -0600

    refactor output tests

--- tests/unit/blocks/test_output_block.py
+++ tests/unit/blocks/test_output_block.py
@@ -10,37 +10,37 @@ def test_output_block_init():
     assert block.file_path == ""
 
 
-def test_output_block_parse_name(basic_parsed_terraform):
-    output_block_content = basic_parsed_terraform["output"]
+def test_output_block_parse_name(raw_terraform_block):
+    output_block_content = raw_terraform_block("output/simple_output.tf", "output")
     output_block = OutputBlock()
     output_block.parse(output_block_content[0], "terraform")
     assert output_block.name == "key_arn"
 
 
-def test_output_block_parse_value(basic_parsed_terraform):
-    output_block_content = basic_parsed_terraform["output"]
+def test_output_block_parse_value(raw_terraform_block):
+    output_block_content = raw_terraform_block("output/simple_output.tf", "output")
     output_block = OutputBlock()
     output_block.parse(output_block_content[0], "terraform")
     assert output_block.value == "${aws_kms_key.key.arn}"
 
 
-def test_output_block_parse_description(basic_parsed_terraform):
-    output_block_content = basic_parsed_terraform["output"]
+def test_output_block_parse_description(raw_terraform_block):
+    output_block_content = raw_terraform_block("output/simple_output.tf", "output")
     output_block = OutputBlock()
     output_block.parse(output_block_content[0], "terraform")
     assert output_block.description == "ARN of the KMS key"
 
 
-def test_output_block_parse_options(basic_parsed_terraform):
-    output_block_content = basic_parsed_terraform["output"]
+def test_output_block_parse_options(raw_terraform_block):
+    output_block_content = raw_terraform_block("output/simple_output.tf", "output")
     output_block = OutputBlock()
     output_block.parse(output_block_content[0], "terraform")
     assert output_block.options == {
         "sensitive": False
     }
 
-def test_output_block_parse_file_path(basic_parsed_terraform):
-    output_block_content = basic_parsed_terraform["output"]
+def test_output_block_parse_file_path(raw_terraform_block):
+    output_block_content = raw_terraform_block("output/simple_output.tf", "output")
     output_block = OutputBlock()
     output_block.parse(output_block_content[0], "terraform")
     assert output_block.file_path == "terraform"
