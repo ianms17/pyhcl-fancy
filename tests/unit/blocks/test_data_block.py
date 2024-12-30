528f2a9905ed7aafe464e59566ee192d257d48ff
ianms17 <ianmstephenson17@gmail.com>
2024-12-30 16:10:59 -0600

    refactor data after fixture change

--- tests/unit/blocks/test_data_block.py
+++ tests/unit/blocks/test_data_block.py
@@ -12,57 +12,57 @@ def test_data_block_init():
     assert block.count is None
 
 
-def test_basic_data_block_parse_data_name(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_data_name(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.data_name == "queue"
 
 
-def test_basic_data_block_parse_data_type(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_data_type(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.data_type == "aws_sqs_queue"
 
 
-def test_basic_data_block_parse_state_path(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_state_path(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.state_path == "data.aws_sqs_queue.queue"
 
 
-def test_basic_data_block_parse_content(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_content(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.content["name"] == "my-queue"
 
 
-def test_basic_data_block_parse_for_each(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_for_each(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.for_each is None
 
 
-def test_basic_data_block_parse_count(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_count(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
-    assert data_block.count == 0
+    assert data_block.count is None
 
 
-def test_basic_data_block_parse_file_path(basic_parsed_terraform, sample_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_basic_data_block_parse_file_path(raw_terraform_block, sample_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", sample_file_node)
     assert data_block.file_path == "terraform"
 
 
-def test_submodule_data_block_parse_state_path(basic_parsed_terraform, submodule_file_node):
-    data_block_list = basic_parsed_terraform["data"]
+def test_submodule_data_block_parse_state_path(raw_terraform_block, submodule_file_node):
+    data_block_list = raw_terraform_block("data/simple_data.tf", "data")
     data_block = DataBlock()
     data_block.parse(data_block_list[0], "terraform", submodule_file_node)
     assert data_block.state_path == "module.test_module.data.aws_sqs_queue.queue"
