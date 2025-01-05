9d976ff0628e00946a0c34d6b49a1343f7fe50d6
ianms17 <ianmstephenson17@gmail.com>
2025-01-05 12:34:51 -0600

    testing for parse utils

--- pyhcl_fancy/parser/utils.py
+++ pyhcl_fancy/parser/utils.py
@@ -16,8 +16,8 @@ def _open_all_tf_files(directory_path: str) -> dict:
     tf_dir = pathlib.Path(directory_path)
     tf_file_content = {}
 
-    for tf_file in tf_dir.rglob("/*.tf"):
+    for tf_file in tf_dir.rglob("./*.tf"):
         with open(tf_file, "r") as f:
-            tf_file_content[tf_file] = hcl2.load(f)
+            tf_file_content[str(tf_file)] = hcl2.load(f)
 
     return tf_file_content
